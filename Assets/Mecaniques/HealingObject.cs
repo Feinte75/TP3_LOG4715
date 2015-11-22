5b8a41e94e8aa81b32f49b7ab9e619704aff3999
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-11-22 14:11:31 -0500

    Init

---
+++ Assets/Mecaniques/HealingObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealingObject : MonoBehaviour {
+
+	public int HP = 50 ;
+	
+	// Entre dans cette fonction lorsqu il y a une collision
+	void OnCollisionEnter(Collision collision) {
+		
+		if(collision.gameObject.GetComponentInParent<CarStatus>() ){ 
+			
+			CarStatus car = collision.gameObject.GetComponent<CarStatus>() ;
+
+
+			GameObject.Destroy (this.gameObject);
+			
+			car.recupererDegat(HP);
+			
+		}
+		
+	}
+}
---
+++ Assets/Mecaniques/Text/LapText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LapText : MonoBehaviour {
+
+	public Text lapText;
+
+	public int lap;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GameObject player = GameObject.Find("Joueur 1");
+		if (player!=null)
+		{
+			CarController playercar = player.GetComponent<CarController>();
+			if (playercar)
+			{
+				GameObject manager = GameObject.Find("Game Manager");
+				CheckpointManager cmanager = manager.GetComponent<CheckpointManager>();
+				lap = cmanager.GetLap(playercar) + 1;	
+				lapText.text ="Lap : " + lap.ToString () + "/3";
+			}
+		}
+	}
+}
