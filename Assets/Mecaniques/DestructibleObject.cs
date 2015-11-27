262e1459eb28bd4151d09b4e9e533b6af6f25140
unknown <jamouc@L4818-15.gigl.polymtl.ca>
2015-11-26 21:00:01 -0500

    Modif terrain

--- Assets/Mecaniques/DestructibleObject.cs
+++ Assets/Mecaniques/DestructibleObject.cs
@@ -6,6 +6,7 @@ public class DestructibleObject : MonoBehaviour {
 	public int damage = 20 ;
 
 	public bool destructible = true;
+	public bool explode = true;
 
 	public float explosionForce = 2;
 	// Entre dans cette fonction lorsqu il y a une collision
@@ -14,14 +15,16 @@ public class DestructibleObject : MonoBehaviour {
 		if(collision.gameObject.GetComponent<CarStatus>() ){ 
 
 			CarStatus car = collision.gameObject.GetComponent<CarStatus>() ;
+			if(explode){
 
-			GameObject prefabExplosion = Resources.Load("Explosion") as GameObject;
-			ExplosionPhysicsForce epf = prefabExplosion.GetComponent<ExplosionPhysicsForce>();
-			epf.explosionForce = explosionForce;
-			car.transform.position = this.transform.position;
-			GameObject explosion = Instantiate (prefabExplosion) as GameObject ;
-			GameObject.Destroy(explosion,3);
-
+				GameObject prefabExplosion = Resources.Load("Explosion") as GameObject;
+				ExplosionPhysicsForce epf = prefabExplosion.GetComponent<ExplosionPhysicsForce>();
+				epf.explosionForce = explosionForce;
+				car.transform.position = this.transform.position;
+				GameObject explosion = Instantiate (prefabExplosion) as GameObject ;
+				GameObject.Destroy(explosion,3);
+			
+			}
 			if(destructible){
 				GameObject.Destroy (this.gameObject);
 			}
